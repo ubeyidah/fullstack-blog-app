c9c7c767b07b9bee86edc09ad25ffe4f48a231e9
ubeyid oumer <ubeyidah@gmail.com>
2024-07-13 00:28:20 +0300

    create login ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layouts/Layout";
 import Posts from "./pages/post/Posts";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Posts />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Route>
       </Routes>
     </BrowserRouter>
