17cc6445dae01d454985c136937e7a20ea39ee2a
ubeyid oumer <ubeyidah@gmail.com>
2024-07-13 22:38:02 +0300

    loading for post detail

--- client/src/pages/post/PostDetail.jsx
+++ client/src/pages/post/PostDetail.jsx
@@ -29,7 +29,31 @@ const PostDetail = () => {
     loadPost();
   }, [id]);
   if (loading) {
-    return <h1 className="py-10">Loading</h1>;
+    return (
+      <div role="status" className="flex flex-col mt-10">
+        <div className="flex items-center justify-center w-full h-72 bg-gray-300 rounded mb-6">
+          <svg
+            className="w-10 h-10 text-gray-200 "
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 18"
+          >
+            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+          </svg>
+        </div>
+        <div className="w-full mt-4">
+          <div className="h-4 bg-gray-200 rounded-full  w-full mb-4"></div>
+          <div className="h-4 bg-gray-200 rounded-full  w-full mb-4"></div>
+          <div className="h-2 bg-gray-200 rounded-full  w-full mb-3"></div>
+          <div className="h-2 bg-gray-200 rounded-full  w-full mb-3"></div>
+          <div className="h-2 bg-gray-200 rounded-full  w-full mb-3"></div>
+          <div className="h-2 bg-gray-200 rounded-full  w-full mb-3"></div>
+          <div className="h-2 bg-gray-200 rounded-full  max-w-[100px] my-5"></div>
+        </div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
   }
   if (error) {
     return <h1>{error}</h1>;
