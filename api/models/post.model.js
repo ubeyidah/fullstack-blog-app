20d378cd29ea7b6ab21ade61bc784d6ea78a6b31
ubeyid oumer <ubeyidah@gmail.com>
2024-07-13 14:40:58 +0300

    create post model

---
+++ api/models/post.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    authorId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Please provide a title for this post."],
+    },
+    summary: {
+      type: String,
+      required: [true, "Please provide a summary for this post."],
+    },
+    content: {
+      type: String,
+      required: [true, "Please provide a main content for this post."],
+    },
+    image: {
+      type: String,
+      required: [true, "Please provide a banner image for this post."],
+    },
+  },
+  { timestamps: true }
+);
+
+const Posts = mongoose.model("Post", postSchema);
+export default Posts;
